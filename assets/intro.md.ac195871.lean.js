552c0c75b751acbfe049581458283b8b9658ecba
Ernesto Canquiz <fdsoil123@gmail.com>
2023-05-06 19:25:41 -0400

    deploy

---
+++ assets/intro.md.ac195871.lean.js
@@ -0,0 +1,1 @@
+import{_ as e,c as a,o as t,O as i}from"./chunks/framework.c499281c.js";const f=JSON.parse('{"title":"Introducción","description":"","frontmatter":{},"headers":[],"relativePath":"intro.md"}'),r={name:"intro.md"},o=i("",4),c=[o];function s(n,l,p,d,u,m){return t(),a("div",null,c)}const h=e(r,[["render",s]]);export{f as __pageData,h as default};
