5fd5ec12f134c8cea00ad0a73c347b1c1052f671
Ernesto Canquiz <fdsoil123@gmail.com>
2023-07-03 12:44:29 -0400

    deploy

---
+++ assets/how-to-use-interfaces.md.bb79af4b.lean.js
@@ -0,0 +1,1 @@
+import{_ as s,c as a,o as n,O as e}from"./chunks/framework.c499281c.js";const d=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"how-to-use-interfaces.md"}'),o={name:"how-to-use-interfaces.md"},l=e("",107),p=[l];function t(c,r,i,y,D,C){return n(),a("div",null,p)}const A=s(o,[["render",t]]);export{d as __pageData,A as default};
